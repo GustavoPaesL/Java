a7c1d4189dfa1964c15737a229241ae59a00a567
GustavoPaesL <gupaes2014@gmail.com>
2024-05-14 19:05:53 -0300

    ArrayList 2D

--- MyProgam/src/Main.java
+++ MyProgam/src/Main.java
@@ -3,16 +3,34 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         
-        ArrayList<String> food = new ArrayList<>();
+        ArrayList<ArrayList<String>> myList = new ArrayList();
 
-        food.add("pizza");
-        food.add("hamburger");
-        food.add("hotdog");
+        ArrayList<String> listA =new ArrayList();
+        listA.add("pasta");
+        listA.add("garlic");
+        listA.add("donuts");
+        
+        ArrayList<String> listB =new ArrayList();
+        listB.add("coke");
+        listB.add("pepsi");
+        listB.add("guarana");
 
-        for(int i=0; i<food.size(); i++){
-            System.out.println(food.get(i));
+        ArrayList<String> listC =new ArrayList();
+        listC.add("apple");
+        listC.add("banana");
+        listC.add("orange");
+
+        myList.add(listA);
+        myList.add(listB);
+        myList.add(listC);
+
+        for(int i=0; i<myList.size(); i++){
+            System.out.println();
+            for(int j=0; j<myList.get(i).size(); j++){
+                System.out.println(myList.get(i).get(j));
+            }
         }
-
+        
     }    
     
 }
