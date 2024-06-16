374619b8269bd7eb9659fbdd7cf897cbc276acf9
GustavoPaesL <gupaes2014@gmail.com>
2024-06-16 16:13:17 -0300

    objects

--- aprendendo/src/Main.java
+++ aprendendo/src/Main.java
@@ -2,41 +2,19 @@
 public class Main {
     public static void main(String[] args) {    
 
-        //printf() = an optional method to control, format, and display text to the console window 
-        //           two arguments = format string + (object/variable/value)
-        //           % [flags] [precision] [width] [conversion-character]
+        // object = an instance of a class that may contain attributes and methods
+        // example: (phone, desk, computer, coffee cup)
 
-        //System.out.printf("This is a format String %d", 111);
-        
-        boolean myBoolean = true;
-        char myChar = '@';
-        String myString = "Bro";
-        int myInt = 50;
-        double myDouble = 1000;
+        Car myCar = new Car();
 
-        // [conversion-character]
-        System.out.printf("%b", myBoolean);
-        System.out.printf("%c", myChar);
-        System.out.printf("%s", myString);
-        System.out.printf("%d", myInt);
-        System.out.printf("%f", myDouble);
+        System.out.println(myCar.make);
+        System.out.println(myCar.model);
+        System.out.println(myCar.year);
+        System.out.println(myCar.color);
+        System.out.println(myCar.price);
 
-        // [width]
-        //minimum numeber of characters to be written as output
-        System.out.printf("Hello %10s", myString);
-
-        //  [precision]
-        //sets number of digits of precision when outputting floating-point values
-        System.out.printf("You have this much money %.2f ", myDouble);
-
-        // [flags]
-        // adds an effect to output based  on the flag added to format specifier
-        // - : left-justify
-        // + : output a plus (+) or minus (-) sign for a numeric value
-        // 0 : numeric values are zero-padded
-        // , : comma grouping separetor if numbers > 1000
-
-        System.out.printf("You have this much money %+,f", myDouble);
+        myCar.drive();
+        myCar.brake();
 
     }    
     
