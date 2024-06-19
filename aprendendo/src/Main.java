eb75ff9bab1be4accfd53352b0219fa6aac2562e
GustavoPaesL <gupaes2014@gmail.com>
2024-06-19 18:22:50 -0300

    toString method

--- aprendendo/src/Main.java
+++ aprendendo/src/Main.java
@@ -1,19 +1,19 @@
 
 public class Main {
-    public static void main(String[] args) {    
 
-        // overloaded constructors = multiple constructors within a class with the same name,
-        //                           but have different parameters
-        //                           name + parameters = signature
 
-        Pizza pizza = new Pizza("thicc crust", "tomato", "mozzarella");
+    public static void main(String[] args) {
+        
+        // toString() = special metohd that all objects inherit.
+        //              that returns a string that "textually represents" an object.
+        //              can be used both implicitly or explicitly
 
-        System.out.println("here are the ingredients of your pizza: ");
-        System.out.println(pizza.bread);
-        System.out.println(pizza.sauce);
-        System.out.println(pizza.cheese);
-        System.out.println(pizza.topping);
+        Car car = new Car();
 
-    }    
-    
+        //System.out.println(car.toString());
+        System.out.println(car);
+
+    }
+
+
 }
